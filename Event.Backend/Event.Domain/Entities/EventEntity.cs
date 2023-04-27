289d561c11fb345137771b2587110d71d75ec445
Vladislav Kostin <70699082+ds-vs@users.noreply.github.com>
2023-04-27 15:23:21 +0300

    Create a clean architecture solution

---
+++ Event.Backend/Event.Domain/Entities/EventEntity.cs
@@ -0,0 +1,26 @@
+﻿using Event.Domain.Enums;
+
+namespace Event.Domain.Entities
+{
+    /// <summary> Сущность описывающая мероприятие. </summary>
+    public class EventEntity
+    {
+        /// <summary> Уникальный идентификатор мероприятия. </summary>
+        public Guid EventId { get; set; }
+
+        /// <summary> Название (заголовок) мероприятия. </summary>
+        public string Title { get; set; } = string.Empty;
+
+        /// <summary> Описание мероприятия. </summary>
+        public string Description { get; set; } = string.Empty;
+
+        /// <summary> Количество человек отметивших мероприятие (дали отклик на событие). </summary>
+        public long Responses { get; set; }
+
+        /// <summary> Дата проведения мероприятия. </summary>
+        public DateTime EventDate { get; set; }
+
+        /// <summary> Статус проведения мероприятия. </summary>
+        public StatusEnum Status { get; set; }
+    }
+}
---
+++ Event.Backend/Event.Domain/Enums/StatusEnum.cs
@@ -0,0 +1,11 @@
+﻿namespace Event.Domain.Enums
+{
+    /// <summary> 
+    /// Перечисление статусов, отображающих актуальность проводимого мероприятия. 
+    /// </summary>
+    public enum StatusEnum
+    {
+        Actual = 0,
+        Completed = 1,
+    }
+}
