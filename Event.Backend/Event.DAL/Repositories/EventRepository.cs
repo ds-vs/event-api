1bb2e9312cc316e65e22634ebaa3e841375d17bf
Vladislav Kostin <70699082+ds-vs@users.noreply.github.com>
2023-04-27 17:57:34 +0300

    Add class repository

---
+++ Event.Backend/Event.DAL/Repositories/EventRepository.cs
@@ -0,0 +1,20 @@
+﻿using Event.DAL.Repositories.Interfaces;
+using Event.Domain.Entities;
+
+namespace Event.DAL.Repositories
+{
+    public class EventRepository : IEventRepository
+    {
+        private readonly EventDbContext _context;
+
+        public EventRepository(EventDbContext context)
+        {
+            _context = context;
+        }
+
+        public IQueryable<EventEntity> Get()
+        {
+            return _context.Events.AsQueryable();
+        }
+    }
+}
---
+++ Event.Backend/Event.DAL/Repositories/Interfaces/IEventRepository.cs
@@ -0,0 +1,12 @@
+﻿using Event.Domain.Entities;
+
+namespace Event.DAL.Repositories.Interfaces
+{
+    /// <summary> Интерфейс репозитория для <see cref="EventEntity"/>. </summary>
+    public interface IEventRepository
+    {
+        /// <summary> Получение всех <see cref="EventEntity"/>. </summary>
+        /// <returns> <see cref="EventEntity"/> коллекция. </returns>
+        IQueryable<EventEntity> Get();
+    }
+}
