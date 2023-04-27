e039bfa56ce7e322fd1b18cb268774533a4a16fc
Vladislav Kostin <70699082+ds-vs@users.noreply.github.com>
2023-04-27 17:21:26 +0300

    Apply first migration

---
+++ Event.Backend/Event.DAL/Migrations/20230427141419_Init.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Event.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class Init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "events",
+                columns: table => new
+                {
+                    event_id = table.Column<Guid>(type: "uuid", nullable: false, defaultValueSql: "uuid_generate_v4()"),
+                    title = table.Column<string>(type: "text", maxLength: 60, nullable: false),
+                    description = table.Column<string>(type: "text", maxLength: 200, nullable: false),
+                    responses = table.Column<long>(type: "bigint", nullable: false, defaultValue: 0L),
+                    event_date = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    status = table.Column<int>(type: "int", nullable: false, defaultValue: 0)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_events", x => x.event_id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "events");
+        }
+    }
+}
